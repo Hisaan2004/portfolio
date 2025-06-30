5ccce37e97c93201823c92ea80dde655f17d4c48
hisaansakhawat <hisaan.sakhawat@gmail.com>
2025-06-30 14:12:24 +0500

    Added new files and Updated files

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,5 +1,5 @@
-'use client';
-import React, { useState } from 'react';
+
+import React from 'react';
 import Text from '@/app/widget/Text'
 import Skillpart from './Skillpart';
 const description=[{
