3ec9a388cf6e227dd40766012365e23e3f2dd5f2
hisaansakhawat <hisaan.sakhawat@gmail.com>
2025-07-08 16:16:17 +0500

    Added and Updated files

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -3,13 +3,13 @@ import { generateBlog } from "@/lib/generateBlog";
 import { saveToMongo } from "@/lib/saveToMongodb";
 import {BLOG_PROMPT} from "@/app/api/blog/blogPrompt";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
   try {
     const blog = await generateBlog(BLOG_PROMPT);
     await saveToMongo(blog);
-    res.status(200).json({ message: "Blog generated and saved", blog });
+    return Response.json({ message: "Blog generated and saved", blog });
   } catch (err) {
     console.error("Error:", err);
-    res.status(500).json({ error: "Failed to generate or save blog" });
+    return Response.json({ error: "Failed to generate or save blog" });
   }
 }
