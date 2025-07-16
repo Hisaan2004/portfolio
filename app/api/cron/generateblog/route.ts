db275d6dfd20a490dbbe19346d80aed079f5dc95
hisaansakhawat <hisaan.sakhawat@gmail.com>
2025-07-16 13:26:13 +0500

    updated files

--- app/api/cron/generateblog/route.ts
+++ app/api/cron/generateblog/route.ts
@@ -1,6 +1,6 @@
 import { generateBlog } from "@/lib/blogMaker/generateBlog";
 import { saveToMongo } from "@/lib/dbHelper/saveToMongodb";
-import { generateBlogPrompt } from "@/lib/blogMaker/generatePrompt";
+import { generateBlogPrompt } from "@/lib/blogMaker/prompt/generatePrompt";
 
 export const revalidate = 0;
 
@@ -9,12 +9,9 @@ export async function GET() {
     const BLOG_PROMPT = await generateBlogPrompt();
     const generatedBlogdata = await generateBlog(BLOG_PROMPT);
     await saveToMongo(generatedBlogdata.data);
-    return Response.json({
-      message: "Blog generated and saved",
-      generatedBlogdata,
-    });
+    return Response.json({ message: "Blog generated and saved", generatedBlogdata });
   } catch (error) {
-    console.error("error", error);
+    console.error("error",error);
     return Response.json({ error: "Failed to generate or save blog" });
   }
 }
