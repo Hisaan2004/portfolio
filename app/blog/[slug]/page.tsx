021ed5290f94006b3aa1323ae2296c7a69535a92
hisaansakhawat <hisaan.sakhawat@gmail.com>
2025-08-07 22:38:57 +0500

    updating file

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -72,7 +72,7 @@ export default async function BlogPage({
 
   return <BlogDetails blog={blog} />;
 }
-*/
+*//*
 import { connectToDB } from "@/app/service/mongodb";
 import { notFound } from "next/navigation";
 import BlogDetails from "./blogDetails";
@@ -151,4 +151,58 @@ export default async function BlogPage({
   if (!blog) return notFound();
 
   return <BlogDetails blog={blog} />;
+}*/
+// File: /app/blog/[slug]/page.tsx
+import { connectToDB } from "@/app/service/mongodb";
+import { notFound } from "next/navigation";
+import BlogDetails from "./blogDetails";
+import { CONFIG } from "@/config";
+type Blog = {
+  title: string;
+  slug: string;
+  date: string;
+  author: string;
+  content: string;
+  metaTitle: string;
+  metaDescription: string;
+  keywords: string[];
+  canonicalUrl: string;
+  ogTitle: string;
+  ogDescription: string;
+  ogImage: string;
+  imageAlt: string;
+};
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+
+  const db = await connectToDB();
+  const blog = await db.collection(CONFIG.COLLECTION_NAME).findOne({ slug });
+
+  if (!blog) {
+    return { title: "Blog Not Found" };
+  }
+
+  return {
+    title: blog.metaTitle,
+    description: blog.metaDescription,
+    openGraph: {
+      title: blog.ogTitle,
+      description: blog.ogDescription,
+      images: [{ url: blog.ogImage }],
+    },
+  };
 }
+
+export default async function BlogPage({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+
+  const db = await connectToDB();
+ // const blog = await db.collection(CONFIG.COLLECTION_NAME).findOne({ slug });
+const blog = await db
+  .collection<Blog>(CONFIG.COLLECTION_NAME)
+  .findOne({ slug }, { projection: { _id: 0 } }) as Blog;
+  if (!blog) return notFound();
+
+  return <BlogDetails blog ={blog} />;
+}
+
